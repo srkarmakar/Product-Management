9be2dee541c4a7845e813aacfa3c63d7bb76ccd9
srkarmakar <167770618+srkarmakar@users.noreply.github.com>
2024-06-21 20:02:55 +0530

    6/21 Product Management

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  template: `
+  <h1>Header Works</h1>
+  `,
+  styles:``
+})
+export class HeaderComponent {
+
+}
