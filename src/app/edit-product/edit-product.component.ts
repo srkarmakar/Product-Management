e45f76541d87acb0abc446118d50ebda9fc64919
srkarmakar <167770618+srkarmakar@users.noreply.github.com>
2024-06-28 19:52:08 +0530

    06/28 Product Management

--- src/app/edit-product/edit-product.component.ts
+++ src/app/edit-product/edit-product.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { ProductServiceService } from '../services/product-service.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-edit-product',
@@ -18,20 +19,21 @@ export class EditProductComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private productService: ProductServiceService,
-    public activatedRoute: ActivatedRoute) { }
+    public activatedRoute: ActivatedRoute,
+    public toastrService: ToastrService) { }
 
   productForm: FormGroup = this.formBuilder.group({
     id: [''],
-    name: [''],
-    description: [''],
-    headline: [''],
-    calories: [''],
-    carbos: [''],
-    fats: [''],
-    proteins: [''],
-    image: ['']
+    name: ['', Validators.required],
+    description: ['', Validators.required],
+    headline: ['', Validators.required],
+    calories: ['', Validators.required],
+    carbos: ['', Validators.required],
+    fats: ['', Validators.required],
+    proteins: ['', Validators.required],
+    image: ['', Validators.required]
   })
-  router=inject(Router);
+  router = inject(Router);
   ngOnInit(): void {
     let productID = this.activatedRoute.snapshot.params["id"]
     this.productService.getProductDetails(productID).subscribe(result =>
@@ -40,9 +42,13 @@ export class EditProductComponent implements OnInit {
   }
 
   editProduct() {
-    this.productService.editProduct(this.productForm.value).subscribe(result =>
-      this.router.navigateByUrl('')
-    )
+    if (this.productForm.valid) {
+      this.productService.editProduct(this.productForm.value).subscribe(result =>
+        this.router.navigateByUrl('')
+      )
+      this.toastrService.success('Product Updated Successfully!', 'Success')
+    } else this.toastrService.error('Please fill all the fields', 'Error')
+
   }
 
 }
